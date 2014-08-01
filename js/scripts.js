e596a9b2c921afd772cefc5e1560bb0bc3b0b78a
ChengLu <saobao@me.com>
2014-08-01 19:50:28 +0800

    repleace google plus one to jiathis

--- js/scripts.js
+++ js/scripts.js
@@ -265,14 +265,14 @@
 
 
     //Google plus
-    (function() {
-        var po = document.createElement('script');
-        po.type = 'text/javascript';
-        po.async = true;
-        po.src = 'https://apis.google.com/js/platform.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(po, s);
-    })();
+//     (function() {
+//         var po = document.createElement('script');
+//         po.type = 'text/javascript';
+//         po.async = true;
+//         po.src = 'https://apis.google.com/js/platform.js';
+//         var s = document.getElementsByTagName('script')[0];
+//         s.parentNode.insertBefore(po, s);
+//     })();
 
     //Google maps
     if (typeof googleMaps !== 'undefined') {
